499e5eddc04a4dc51eed4a504e2c5b0fd39a8d34
Abhinandh S <ugabhi@proton.me>
2024-11-28 20:23:14 +0530

    Refactoring

--- main.ts
+++ main.ts
@@ -1,21 +1,20 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 import { join } from "https://deno.land/std/path/mod.ts";
-import { readDir } from "https://deno.land/std/fs/mod.ts";
 
-// Path to the folder containing your built book files
-const bookDir = "./book";
+// Path to your static book directory (mdBook output)
+const bookDir = "./book";  // Make sure to adjust this if it's different
 
 async function handler(req: Request): Promise<Response> {
   const url = new URL(req.url);
-  const filePath = join(bookDir, url.pathname === "/" ? "index.html" : url.pathname);
+  let filePath = join(bookDir, url.pathname === "/" ? "index.html" : url.pathname);
 
   try {
     const file = await Deno.readFile(filePath);
     const ext = filePath.split('.').pop();
 
     const contentType = ext === "html" ? "text/html" :
-                       ext === "css" ? "text/css" :
-                       ext === "js" ? "application/javascript" : "application/octet-stream";
+                        ext === "css" ? "text/css" :
+                        ext === "js" ? "application/javascript" : "application/octet-stream";
 
     return new Response(file, { status: 200, headers: { "Content-Type": contentType } });
   } catch {
@@ -24,3 +23,4 @@ async function handler(req: Request): Promise<Response> {
 }
 
 serve(handler);
+
